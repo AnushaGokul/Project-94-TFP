a0240a82b32ff87a5047bc7c7986fa80dee78c02
AnushaGokul <71561519+AnushaGokul@users.noreply.github.com>
2021-02-12 20:09:52 +0530

    Update Room.js

--- Room.js
+++ Room.js
@@ -21,3 +21,13 @@ localStorage.removeItem("user_name");
 localStorage.removeItem("room_name");
     window.location = "index.html";
 }
+function addRoom()
+{
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
+
+    localStorage.setItem("room_name", room_name);
+}
